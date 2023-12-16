41023a72ddcb790c0ddef8e15baa128c47e4c010
Aadhar gaur <aadhar41@gmail.com>
2023-12-16 17:01:24 +0530

    008 Attendees and Pagination

--- app/Http/Controllers/Api/EventController.php
+++ app/Http/Controllers/Api/EventController.php
@@ -15,7 +15,7 @@ class EventController extends Controller
      */
     public function index()
     {
-        return EventResource::collection(Event::with('user', 'attendees')->get());
+        return EventResource::collection(Event::with('user', 'attendees')->paginate());
     }
 
     /**
