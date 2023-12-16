65c6f7be51100037406421c2988898db6b7bf3a8
Aadhar gaur <aadhar41@gmail.com>
2023-12-16 15:00:15 +0530

    003 Seeding Data for the REST API

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\Api\AttendeeController;
+use App\Http\Controllers\Api\EventController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -17,3 +19,7 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::apiResource('events', EventController::class);
+
+Route::apiResource('events.attendees', AttendeeController::class)->scoped(['attendee' => 'event']);
