b1e216877cd9ebc4eefd17e12acf7b8c404ae026
Aadhar gaur <aadhar41@gmail.com>
2023-12-16 16:30:38 +0530

    007 API Resources - Controlling JSON Response

--- app/Http/Controllers/Api/EventController.php
+++ app/Http/Controllers/Api/EventController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Http\Resources\EventResource;
 use App\Models\Event;
 use App\Models\User;
 use Illuminate\Http\Request;
@@ -14,7 +15,7 @@ class EventController extends Controller
      */
     public function index()
     {
-        return Event::all();
+        return EventResource::collection(Event::with('user', 'attendees')->get());
     }
 
     /**
@@ -32,7 +33,7 @@ class EventController extends Controller
             'user_id' => 1
         ]);
 
-        return $event;
+        return new EventResource($event);
     }
 
     /**
@@ -40,7 +41,8 @@ class EventController extends Controller
      */
     public function show(Event $event)
     {
-        return $event;
+        $event->load('user', 'attendees');
+        return new EventResource($event);
     }
 
     /**
@@ -57,7 +59,7 @@ class EventController extends Controller
             ]),
         );
 
-        return $event;
+        return new EventResource($event);
     }
 
     /**
