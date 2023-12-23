f864fa594e7ef309bfef9f5c020e8a743897d7d5
Aadhar gaur <aadhar41@gmail.com>
2023-12-23 23:23:43 +0530

    swagger api documentation for Auth & Events APIs

--- app/Http/Controllers/Api/EventController.php
+++ app/Http/Controllers/Api/EventController.php
@@ -24,7 +24,94 @@ class EventController extends Controller
     }
 
     /**
-     * Display a listing of the resource.
+     * @OA\Get(
+     *    path="/events",
+     *    operationId="index",
+     *    tags={"Events"},
+     *    summary="Get list of events",
+     *    description="Get list of events",
+     *    @OA\Parameter(
+     *        name="include",
+     *        in="query",
+     *        description="relationship data i.e. attendees,user",
+     *        required=false,
+     *        @OA\Schema(type="string")
+     *    ),
+     *    @OA\Parameter(
+     *        name="page",
+     *        in="query",
+     *        description="the page number",
+     *        required=false,
+     *        @OA\Schema(type="integer")
+     *    ),
+     *     @OA\Response(response="404", description="Error: Not Found. The route events could not be found."),
+     *     @OA\Response(
+     *          response=200, description="Success",
+     *          @OA\JsonContent(
+     *             type="object",
+     *             @OA\Property(
+     *                 property="data",
+     *                 type="array",
+     *                 format="object",
+     *                 @OA\Items(
+     *                      @OA\Property(
+     *                         property="id",
+     *                         type="integer",
+     *                         example="3"
+     *                      ),
+     *                      @OA\Property(
+     *                         property="name",
+     *                         type="string",
+     *                         example="Aut est minima."
+     *                      ),
+     *                      @OA\Property(
+     *                         property="description",
+     *                         type="string",
+     *                         example="Ipsam iste hic ut repellat totam est esse ad."
+     *                      ),
+     *                      @OA\Property(
+     *                         property="start_time",
+     *                         type="string",
+     *                         example="2023-12-16 13:47:06"
+     *                      ),
+     *                      @OA\Property(
+     *                         property="end_time",
+     *                         type="string",
+     *                         example="2024-02-11 21:18:46"
+     *                      ),
+     *                 ),
+     *                  @OA\Items(
+     *                      @OA\Property(
+     *                         property="id",
+     *                         type="integer",
+     *                         example="191"
+     *                      ),
+     *                      @OA\Property(
+     *                         property="name",
+     *                         type="string",
+     *                         example="Aut est minima."
+     *                      ),
+     *                      @OA\Property(
+     *                         property="description",
+     *                         type="string",
+     *                         example="Ipsam iste hic ut repellat totam est esse ad."
+     *                      ),
+     *                      @OA\Property(
+     *                         property="start_time",
+     *                         type="string",
+     *                         example="2023-12-16 13:47:06"
+     *                      ),
+     *                      @OA\Property(
+     *                         property="end_time",
+     *                         type="string",
+     *                         example="2024-02-11 21:18:46"
+     *                      ),
+     *                 )
+     *             )
+     *          )
+     *     )
+
+     *  )
      */
     public function index()
     {
@@ -35,7 +122,79 @@ class EventController extends Controller
 
 
     /**
-     * Store a newly created resource in storage.
+     * @OA\Post(
+     *      path="/events?include=user,attendees",
+     *      operationId="store",
+     *      tags={"Events"},
+     *      summary="Store/Create event in DB",
+     *      description="Store/Create event in DB",
+     *      @OA\RequestBody(
+     *         required=true,
+     *         @OA\JsonContent(
+     *            required={"name", "start_time", "end_time"},
+     *            @OA\Property(property="name", type="string", format="string", example="Test event Title"),
+     *            @OA\Property(property="start_time", type="string", format="string", example="2023-12-01 15:00:00"),
+     *            @OA\Property(property="end_time", type="string", format="string", example="2023-12-31 15:00:00"),
+     *         ),
+     *          @OA\MediaType(
+     *            mediaType="multipart/form-data",
+     *            @OA\Schema(
+     *               type="object",
+     *               required={"name", "start_time", "end_time"},
+     *               @OA\Property(property="name", type="string", format="string", example="Test event Title"),
+     *               @OA\Property(property="start_time", type="string", format="string", example="2023-12-01 15:00:00"),
+     *               @OA\Property(property="end_time", type="string", format="string", example="2023-12-31 15:00:00"),
+     *            ),
+     *        ),
+     *      ),
+     *     @OA\Response(response="401", description="Unauthenticated."),
+     *     @OA\Response(response="404", description="Error: Not Found. The route events could not be found."),
+     *    @OA\Response(
+     *          response=201, description="Success",
+     *          @OA\JsonContent(
+     *             type="object",
+     *             @OA\Property(
+     *                 property="data",
+     *                 type="array",
+     *                 format="object",
+     *                 example={
+     *                  "id": "3",
+     *                  "name": "Aut est minima.",
+     *                  "description": "Ipsam iste hic ut repellat totam est esse ad.",
+     *                  "start_time": "2023-12-16 13:47:06",
+     *                  "end_time": "2024-02-11 21:18:46",
+     *                },
+     *                 @OA\Items(
+     *                      @OA\Property(
+     *                         property="id",
+     *                         type="integer",
+     *                         example="191"
+     *                      ),
+     *                      @OA\Property(
+     *                         property="name",
+     *                         type="string",
+     *                         example="Aut est minima."
+     *                      ),
+     *                      @OA\Property(
+     *                         property="description",
+     *                         type="string",
+     *                         example="Ipsam iste hic ut repellat totam est esse ad."
+     *                      ),
+     *                      @OA\Property(
+     *                         property="start_time",
+     *                         type="string",
+     *                         example="2023-12-16 13:47:06"
+     *                      ),
+     *                      @OA\Property(
+     *                         property="end_time",
+     *                         type="string",
+     *                         example="2024-02-11 21:18:46"
+     *                      ),
+     *                 )
+     *             )
+     *          )
+     *     )
+     *  )
      */
     public function store(Request $request)
     {
@@ -51,8 +210,66 @@ class EventController extends Controller
         return new EventResource($this->loadRelationships($event));
     }
 
+
     /**
-     * Display the specified resource.
+     * @OA\Get(
+     *    path="/events/{id}",
+     *    operationId="show",
+     *    tags={"Events"},
+     *    summary="Get Event Detail",
+     *    description="Get Event Detail",
+     *    @OA\Parameter(name="id", in="path", description="Id of Event", required=true,
+     *        @OA\Schema(type="integer")
+     *    ),
+     *     @OA\Response(response="401", description="Unauthenticated."),
+     *     @OA\Response(response="404", description="Error: Not Found. The route events could not be found."),
+     *     @OA\Response(
+     *          response=200, description="Success",
+     *          @OA\JsonContent(
+     *             type="object",
+     *             @OA\Property(
+     *                 property="data",
+     *                 type="array",
+     *                 format="object",
+     *                 example={
+     *                  "id": "3",
+     *                  "name": "Aut est minima.",
+     *                  "description": "Ipsam iste hic ut repellat totam est esse ad.",
+     *                  "start_time": "2023-12-16 13:47:06",
+     *                  "end_time": "2024-02-11 21:18:46",
+     *                },
+     *                 @OA\Items(
+     *                      @OA\Property(
+     *                         property="id",
+     *                         type="integer",
+     *                         example="191"
+     *                      ),
+     *                      @OA\Property(
+     *                         property="name",
+     *                         type="string",
+     *                         example="Aut est minima."
+     *                      ),
+     *                      @OA\Property(
+     *                         property="description",
+     *                         type="string",
+     *                         example="Ipsam iste hic ut repellat totam est esse ad."
+     *                      ),
+     *                      @OA\Property(
+     *                         property="start_time",
+     *                         type="string",
+     *                         example="2023-12-16 13:47:06"
+     *                      ),
+     *                      @OA\Property(
+     *                         property="end_time",
+     *                         type="string",
+     *                         example="2024-02-11 21:18:46"
+     *                      ),
+     *                 )
+     *             )
+     *          )
+     *     )
+     *       )
+     *  )
      */
     public function show(Event $event)
     {
@@ -61,6 +278,72 @@ class EventController extends Controller
 
     /**
      * Update the specified resource in storage.
+     */
+    /**
+     * @OA\Put(
+     *     path="/events/{id}",
+     *     operationId="update",
+     *     tags={"Events"},
+     *     summary="Update event in DB",
+     *     description="Update event in DB",
+     *     @OA\Parameter(name="id", in="path", description="Id of Event", required=true,
+     *         @OA\Schema(type="integer")
+     *     ),
+     *     @OA\RequestBody(
+     *        required=true,
+     *        @OA\JsonContent(
+     *           required={"name"},
+     *            @OA\Property(property="name", type="string", format="string", example="Test event Title"),
+     *        ),
+     *     ),
+     *     @OA\Response(response="401", description="Unauthenticated."),
+     *     @OA\Response(response="404", description="Error: Not Found. The route events could not be found."),
+     *     @OA\Response(
+     *          response=200, description="Success",
+     *          @OA\JsonContent(
+     *             type="object",
+     *             @OA\Property(
+     *                 property="data",
+     *                 type="array",
+     *                 format="object",
+     *                 example={
+     *                  "id": "3",
+     *                  "name": "Aut est minima.",
+     *                  "description": "Ipsam iste hic ut repellat totam est esse ad.",
+     *                  "start_time": "2023-12-16 13:47:06",
+     *                  "end_time": "2024-02-11 21:18:46",
+     *                },
+     *                 @OA\Items(
+     *                      @OA\Property(
+     *                         property="id",
+     *                         type="integer",
+     *                         example="191"
+     *                      ),
+     *                      @OA\Property(
+     *                         property="name",
+     *                         type="string",
+     *                         example="Aut est minima."
+     *                      ),
+     *                      @OA\Property(
+     *                         property="description",
+     *                         type="string",
+     *                         example="Ipsam iste hic ut repellat totam est esse ad."
+     *                      ),
+     *                      @OA\Property(
+     *                         property="start_time",
+     *                         type="string",
+     *                         example="2023-12-16 13:47:06"
+     *                      ),
+     *                      @OA\Property(
+     *                         property="end_time",
+     *                         type="string",
+     *                         example="2024-02-11 21:18:46"
+     *                      ),
+     *                 )
+     *             )
+     *          )
+     *     )
+     *  )
      */
     public function update(Request $request, Event $event)
     {
@@ -82,7 +365,27 @@ class EventController extends Controller
     }
 
     /**
-     * Remove the specified resource from storage.
+     * @OA\Delete(
+     *    path="/events/{id}",
+     *    operationId="destroy",
+     *    tags={"Events"},
+     *    summary="Delete Event",
+     *    description="Delete Event",
+     *    @OA\Parameter(name="id", in="path", description="Id of Event", required=true,
+     *        @OA\Schema(type="integer")
+     *    ),
+     *    @OA\Response(response="401", description="Unauthenticated."),
+     *    @OA\Response(response="404", description="Error: Not Found. The route events could not be found."),
+     *    @OA\Response(
+     *         response=200,
+     *         description="Success",
+     *         @OA\JsonContent(
+     *          @OA\Property(property="status_code", type="integer", example="200"),
+     *          @OA\Property(property="data",type="object")
+     *         ),
+     *    )
+     *      )
+     *  )
      */
     public function destroy(Event $event)
     {
--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -6,6 +6,24 @@ use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
 
+/**
+ * @OA\Info(
+ *     version="1.0.0",
+ *     title="Event Management Api Documentation",
+ *     description="Event Management Api Documentation",
+ *     @OA\Contact(
+ *         name="Aadhar gaur",
+ *         email="aadhar41@gmail.com"
+ *     ),
+ *     @OA\License(
+ *         name="Apache 2.0",
+ *         url="http://www.apache.org/licenses/LICENSE-2.0.html"
+ *     )
+ * ),
+ * @OA\Server(
+ *     url="http://events-management-app.test/api/",
+ * ),
+ */
 class Controller extends BaseController
 {
     use AuthorizesRequests, ValidatesRequests;
