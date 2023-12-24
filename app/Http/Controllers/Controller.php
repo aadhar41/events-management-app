4d3b5a0aa2da43e1d9b1f3398a0144605e2ac89d
Aadhar gaur <aadhar41@gmail.com>
2023-12-24 11:35:50 +0530

    Swagger documentation updated

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -10,7 +10,7 @@ use Illuminate\Routing\Controller as BaseController;
  * @OA\Info(
  *     version="1.0.0",
  *     title="Event Management Api Documentation",
- *     description="Event Management Api Documentation",
+ *     description="<b>Api Documentation For</b>, Services responsible for getting the list of all events, get a single event details, creating, updating & deleting an event in database as well as attendee module i.e. getting all attendees for an event, attend an event, get details of an event's attendee & unAttend an Event.",
  *     @OA\Contact(
  *         name="Aadhar gaur",
  *         email="aadhar41@gmail.com"
